1680a1f4d2f63f43490d62ab4a0e97b24efe5ce1
David Atehortua <atehortua1907@gmail.com>
2020-01-14 23:13:57 -0500

    Config.yaml: se agregan dos items por cada site (article_boy, article_title) news_page_objects: Se hace uso de POO herencia en python

--- 1-MinTic_IA/3-IngenieraDeDatos/16_17-WebScrapperg/news_page_objects.py
+++ 1-MinTic_IA/3-IngenieraDeDatos/16_17-WebScrapperg/news_page_objects.py
@@ -2,13 +2,26 @@ import bs4
 import requests
 from common import config
 
-class HomePage:
+class NewsPage:
 
     def __init__(self, news_site_uid, url):
         self._config = config()['news_sites'][news_site_uid]
         self._queries = self._config['queries']
         self._html = None
         self._visit(url)
+
+    def _select(self, query_string):
+        return self._html.select(query_string)
+
+    def _visit(self, url):
+        response = requests.get(url)
+        response.raise_for_status() #Metodo que avienta un error si la solitud no concluye correctamente
+        self._html = bs4.BeautifulSoup(response.text, 'html.parser')
+
+class HomePage(NewsPage): # HERENCIA
+
+    def __init__(self, news_site_uid, url):
+        super().__init__(news_site_uid, url) #Constructor de la clase padre
 
     @property
     def article_links(self):
@@ -18,11 +31,18 @@ class HomePage:
                 link_list.append(link)
         
         return set(link['href'] for link in link_list) # se puede leer: queremos el atributo 'href' por cada link en la lista de links
-    
-    def _select(self, query_string):
-        return self._html.select(query_string)
 
-    def _visit(self, url):
-        response = requests.get(url)
-        response.raise_for_status() #Metodo que avienta un error si la solitud no concluye correctamente
-        self._html = bs4.BeautifulSoup(response.text, 'html.parser')
+class ArticlePage(NewsPage):
+
+    def __init__(self, news_site_uid, url):
+        super().__init__(news_site_uid, url) #Constructor de la clase padre
+
+    @property
+    def body(self):
+        result = self._select(self._queries['article_body'])        
+        return result[0].text if len(result) else ''
+
+    @property
+    def title(self):
+        result = self._select(self._queries['article_title'])        
+        return result[0].text if len(result) else ''
